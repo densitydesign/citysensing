023330de751f092e168d6308b6165f35082030a4
giorgiocaviglia <giorgio.caviglia@gmail.com>
2013-06-25 23:43:14 +0200

    fixed gitignore

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 // initialize our faux database
 var request = require('request'),
-	baseUrl = 'http://80.18.73.38:8175/0.3',
-	//baseUrl = 'http://156.54.107.76:8003/0.3',
+	//baseUrl = 'http://80.18.73.38:8175/0.3',
+	baseUrl = 'http://156.54.107.76:8003/0.3',
 	headers = { 'content-type': 'text/json' };
 
 exports.map = function (req, res) {
